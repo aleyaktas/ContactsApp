f707dc42ae2b3160ccfe1eb80271258e67c1b374
Aleyna Aktaş <aleynaaktas627@gmail.com>
2023-07-22 23:17:52 +0300

    Initial Commit

---
+++ ContactsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsApp
+//
+//  Created by Aleyna Aktaş on 22.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

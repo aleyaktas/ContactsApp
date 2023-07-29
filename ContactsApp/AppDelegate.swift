a451686b10d5ec45251aee540fc228ceefc36645
Aleyna Aktaş <aleynaaktas627@gmail.com>
2023-07-30 00:37:01 +0300

    Add core data

--- ContactsApp/AppDelegate.swift
+++ ContactsApp/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -30,7 +31,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+            
+            let container = NSPersistentContainer(name: "ContactsApp")
+        
+            container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+                if let error = error as NSError? {
+                    fatalError("Unresolved error \(error), \(error.userInfo)")
+                }
+            })
+            return container
+        }()
+        
+        // MARK: - Core Data Saving support
+        
+        func saveContext () {
+            let context = persistentContainer.viewContext
+            
+            if context.hasChanges {
+                do {
+                    try context.save()
+                } catch {
+                    let nserror = error as NSError
+                    fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+                }
+            }
+        }
 
 }
 
